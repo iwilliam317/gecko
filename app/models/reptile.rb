8ab942fc732fb87be2bf8956d8584d58f8eb861b
William <willshinji@gmail.com>
2018-02-28 22:33:55 -0300

    using best-practices for models in rails

--- app/models/reptile.rb
+++ app/models/reptile.rb
@@ -1,11 +1,10 @@
 class Reptile < ApplicationRecord
-	validates_presence_of :name, :birthday, :gender
-	
-	has_attached_file :avatar, style: { medium: "300x300#", thumb: "50x50#" }
-
+	validates :name, :birthday, :gender, presence: true
 	#validates_attachment_presence     :avatar
 	validates_attachment_size         :avatar, :less_than => 5.megabyte
 	validates_attachment_content_type :avatar, :content_type => %w(image/png image/jpeg  image/jpeg image/gif image/tiff)
+	
+	has_attached_file :avatar, style: { medium: "300x300#", thumb: "50x50#" }
 
 	scope :male_only, lambda {where("gender = ?", "M")}
 	scope :female_only, lambda {where("gender = ?", "F")}
